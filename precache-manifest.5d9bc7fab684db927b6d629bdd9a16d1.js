2103e2cd8a853667f893d02ba4ebd54cd1c515e1
Nelson Onuoha <no_student@yahoo.com>
2020-06-25 16:15:23 +0100

    Updates

---
+++ precache-manifest.5d9bc7fab684db927b6d629bdd9a16d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f9242c5c23cddffaa5c9e6ba41fe3f",
+    "url": "/ff-survey/index.html"
+  },
+  {
+    "revision": "e816d8de697b8293cfb7",
+    "url": "/ff-survey/static/css/2.ee0a9b0d.chunk.css"
+  },
+  {
+    "revision": "53f0df636c12651d1b0c",
+    "url": "/ff-survey/static/css/main.6a72d5f8.chunk.css"
+  },
+  {
+    "revision": "e816d8de697b8293cfb7",
+    "url": "/ff-survey/static/js/2.b545d6ac.chunk.js"
+  },
+  {
+    "revision": "53f0df636c12651d1b0c",
+    "url": "/ff-survey/static/js/main.5d844812.chunk.js"
+  },
+  {
+    "revision": "e0d1468c5f464304c010",
+    "url": "/ff-survey/static/js/runtime-main.d0583765.js"
+  }
+]);
