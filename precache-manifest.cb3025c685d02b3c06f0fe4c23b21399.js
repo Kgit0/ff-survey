323f89e01249f225180504eaead0ec8b466f4c90
Nelson Onuoha <no_student@yahoo.com>
2020-07-02 09:03:53 +0100

    Updates

--- precache-manifest.cb3025c685d02b3c06f0fe4c23b21399.js
+++ precache-manifest.cb3025c685d02b3c06f0fe4c23b21399.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "25b5dd7fabed381da75c8f7305f5b5db",
+    "revision": "2a02b54a0f79bd14cdfd730343a558d9",
     "url": "/ff-survey/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ff-survey/static/css/2.ee0a9b0d.chunk.css"
   },
   {
-    "revision": "747d9bef2de26c29655d",
+    "revision": "719bc0eb663900c5cee6",
     "url": "/ff-survey/static/css/main.6a72d5f8.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ff-survey/static/js/2.54182a30.chunk.js"
   },
   {
-    "revision": "747d9bef2de26c29655d",
-    "url": "/ff-survey/static/js/main.de947937.chunk.js"
+    "revision": "719bc0eb663900c5cee6",
+    "url": "/ff-survey/static/js/main.941eb67f.chunk.js"
   },
   {
     "revision": "e0d1468c5f464304c010",
