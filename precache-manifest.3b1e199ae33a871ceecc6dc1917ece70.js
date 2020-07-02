f2997da046eadfd1c0c7edf550e6ce76fec9c190
Nelson Onuoha <no_student@yahoo.com>
2020-07-02 08:47:52 +0100

    Updates

---
+++ precache-manifest.3b1e199ae33a871ceecc6dc1917ece70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ba5d4e154ef814753ecfcac52fe015",
+    "url": "/ff-survey/index.html"
+  },
+  {
+    "revision": "6928a34d12fbb794a692",
+    "url": "/ff-survey/static/css/2.ee0a9b0d.chunk.css"
+  },
+  {
+    "revision": "4b2fe15fb8716a42f6cc",
+    "url": "/ff-survey/static/css/main.6a72d5f8.chunk.css"
+  },
+  {
+    "revision": "6928a34d12fbb794a692",
+    "url": "/ff-survey/static/js/2.54182a30.chunk.js"
+  },
+  {
+    "revision": "4b2fe15fb8716a42f6cc",
+    "url": "/ff-survey/static/js/main.04f047b2.chunk.js"
+  },
+  {
+    "revision": "e0d1468c5f464304c010",
+    "url": "/ff-survey/static/js/runtime-main.d0583765.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55c0c111d9dc8c0f08a669077d9f4751.js"
+  "/ff-survey/precache-manifest.3b1e199ae33a871ceecc6dc1917ece70.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ff-survey/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
